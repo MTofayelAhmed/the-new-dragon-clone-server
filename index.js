28907a82412691fdb869a8d13604d40b65a0135d
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-04-30 17:13:21 +0600

    commit done

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 const express = require('express')
 const categories = require('./Data/categories.json')
+const news = require('./Data/news.json')
+
+
+
 const cors = require('cors')
 const app = express();
 const port = 3000
+
+
+app.get('/news', (req, res)=> {
+  res.send(news)
+})
+
+app.get('/news/:id', (req, res)=>{
+  const id = req.params.id; 
+  console.log(id)
+  const selectedNews= news.find(n => n._id === id) 
+  res.send(selectedNews)
+})
+
+
+
 app.get('/', (req, res)=> {
   res.send('dragon is running on the pitch')
 })
 app.use(cors())
 app.get('/categories', (req, res)=> {
   res.send(categories)
+})
+
+
+app.get('/categories/:id', (req, res)=> {
+  const id = req.params.id;
+  if(id == 0){
+    res.send(news)
+  }
+  else{
+    console.log(id)
+    const newsCategory = news.filter(n => n.category_id === id)
+    res.send(newsCategory)
+  }
+ 
 })
 
 
